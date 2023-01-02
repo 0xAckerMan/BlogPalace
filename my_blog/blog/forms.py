59ba29e5cd42211bad733873b427c2d15c4573bd
0xAckerMan <joelkores1@gmail.com>
2023-01-02 23:02:57 +0300

    The comment module

--- my_blog/blog/forms.py
+++ my_blog/blog/forms.py
@@ -11,3 +11,5 @@ class CommentForm(forms.modelForm):
     def __init__(self, *args, **kwargs):
         supper(CommentForm, self).__init__(*args, **kwargs)
         self.fields['name'].widget.attrs = {'placeholder': 'Enter name','class': 'form-control'}
+        self.fields['email'].widget.attrs = {'placeholder': 'Enter email', 'class':'form-control'}
+        self.fields['body'].widget.attrs = {'placeholder': 'Coment here...', 'class':'form-control','rows':'5'}
